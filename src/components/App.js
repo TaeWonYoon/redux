a71f59f0015decbe12cb30d72ec4ad1cb99ceeed
Your Name <you@example.com>
2020-02-27 08:41:55 +0900

    redux

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React, { Component} from 'react';
+import PropTypes from 'prop-types';
+import Counter from './Counter';
+const propTypes = {
+};
+
+const defaultProps = {
+};
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return(
+            <div>
+              <Counter/>
+            </div>
+        );
+    }
+}
+App.propTypes = propTypes;
+
+App.defaultProps = defaultProps;
+
+export default App;
